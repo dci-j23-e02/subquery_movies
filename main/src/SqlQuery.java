78550a91a192640c22ebcf44e00c744ef3610870
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-03-04 14:11:13 +0100

    init commit

--- main/src/SqlQuery.java
+++ main/src/SqlQuery.java
@@ -1,7 +1,14 @@
 package src;
 
 public enum SqlQuery {
-  FIND_MOVIES_WITH_ACTOR("SELECT * FROM moviestable WHERE EXISTS(SELECT 1 FROM actors WHERE actors.movie_id = moviestable.id AND actors.name = ?)");
+  FIND_MOVIES_WITH_ACTOR(
+      "SELECT * FROM moviestable WHERE EXISTS"
+      + "(SELECT 1 FROM actors WHERE actors.movie_id = moviestable.id AND actors.name = ?)"
+  ),
+  FIND_ACTORS_IN_GENRE(
+      "SELECT name FROM actors WHERE movie_id IN "
+          + "(SELECT id FROM moviestable WHERE genre=?)"
+  );
 
   public final String query;
 
