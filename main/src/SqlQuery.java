ddb52ef053169a805747914b5af8d6ab209fd5aa
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-03-04 14:27:55 +0100

    init commit

--- main/src/SqlQuery.java
+++ main/src/SqlQuery.java
@@ -8,6 +8,10 @@ public enum SqlQuery {
   FIND_ACTORS_IN_GENRE(
       "SELECT name FROM actors WHERE movie_id IN "
           + "(SELECT id FROM moviestable WHERE genre=?)"
+  ),
+  FIND_ACTORS_FOR_MOVIES_NOT_IN_YEAR(
+      "SELECT name FROM actors WHERE movie_id NOT IN"
+        +"(SELECT id FROM moviestable WHERE release_year < ?)"
   );
 
   public final String query;
